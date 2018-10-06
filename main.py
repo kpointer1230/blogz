249b39221201aec20e7224cb80d70b5de52d3c4b
Krystal <kpointer1230@att.net>
2018-10-05 21:54:43 -0500

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, redirect, render_template, flash
+from flask_sqlalchemy import SQLAlchemy 
+import cgi
+import flask
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Ktp1206@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+tasks = []
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+    return render_template('')
