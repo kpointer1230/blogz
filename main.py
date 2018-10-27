62962e82ac6593cf532b8af6e3e16461b2f080cb
Krystal <kpointer1230@att.net>
2018-10-26 22:12:24 -0500

    added password and username requirements

--- main.py
+++ main.py
@@ -9,42 +9,68 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:code123@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = '1112842616kk'
+
 
     #model class for database
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text(560))
-
-    #model class for user
-class User(db.Model):
-    owner_id = db.Column(db.Integer, primary_key=True)    
-    owner_title = db.Column(db.String(120))
-    owner_body = db.Column(db.Text(560 ))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-    #Initialize Blog Class
-    def __init__(self, title, body):
+#Initialize Blog Class
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
+    def __repr__(self):
+        return '<Blog %r>' % self.title    
+
+
+    #model class for user(owner/user = author)
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)    
+    user_username = db.Column(db.String(120))
+    user_password = db.Column(db.Text(560))
+    user_blogs = db.relationship('Blog', backref='owner')
+
+    #initialize user class for user to have a username and password
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
     #string representation of the blog objects
     def __repr__(self):
-        return '<Blog %r>' % self.name
+        return '<User %r>' % self.username
+
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'index', 'blog_entry', 'logout']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+
+#login route handler functions
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = requset.form['username']
 
 
 #signup route handler functions
 @app.route('/signup', methods=['POST', 'GET'])
 def singleUser(signup):
 
-#login route handler functions
-@app.route('/login', methods=['POST', 'GET'])
-def login():
 
 #index route handler functions
 @app.route('/index', methods=['POST', 'GET'])  
 def index():
-    if request.method == 'POST':    
+    if request.method == 'POST': 
+           
 
 #new_posts route handler functions
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -52,6 +78,7 @@ def index():
 
     if request.method == 'GET':
         return render_template('new_posts.html', title='Add Blog Entry')
+
 
 #blog_entry route handler functions
 @app.route('/blog', methods=['POST','GET'])
